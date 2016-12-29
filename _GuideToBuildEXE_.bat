76f8e15d4cc57f6c8b04fc63fc49d38fa44469ab
hossam-magdy <hossam6236@gmail.com>
2016-12-29 02:43:06 +0200

    Update GuideToBuildEXE bat

--- _GuideToBuildEXE_.bat
+++ _GuideToBuildEXE_.bat
@@ -1,17 +1,26 @@
 @echo off
-
-echo to build the exe file, download Ahk2Exe (recommended this version):
-echo Download page: https://github.com/hossam-magdy/FB-TimeSaver/releases/tag/Ahk2Exe-AIO
-echo Direct link:   https://github.com/hossam-magdy/FB-TimeSaver/archive/Ahk2Exe-AIO.zip
-::start "" "https://github.com/hossam-magdy/FB-TimeSaver/releases/tag/Ahk2Exe-AIO"
-::start "" "https://github.com/hossam-magdy/FB-TimeSaver/archive/Ahk2Exe-AIO.zip"
-
-
-::taskkill /F /IM FB_TimeSaver.exe >nul 2>&1
-
-::".\Ahk2Exe.exe" /in FB_TimeSaver.ahk /out FB_TimeSaver.exe
-
-::start "" FB_TimeSaver.exe
-
-::::echo Done.
-::::pause
+SETLOCAL
+SET Ahk2Exe=.\Ahk2Exe.exe
+if not exist "%Ahk2Exe%" SET Ahk2Exe=.\AutoHotkey\Ahk2Exe.exe
+if not exist "%Ahk2Exe%" SET Ahk2Exe=.\.AutoHotkey\Ahk2Exe.exe
+if exist "%Ahk2Exe%" (
+	taskkill /F /IM FB_TimeSaver.exe >nul 2>&1
+	"%Ahk2Exe%" /in FB_TimeSaver.ahk /out FB_TimeSaver.exe
+	start "" FB_TimeSaver.exe
+	::echo Done.
+	::pause
+	exit
+) else (
+	if exist ".\Ahk2Exe_AIO.exe" (
+		start "" ".\Ahk2Exe_AIO.exe"
+		exit
+	) else (
+		echo.
+		echo Download Ahk2Exe "recommended this version", to build the exe file, :
+		echo    Page:     https://github.com/hossam-magdy/FB-TimeSaver/releases/tag/Ahk2Exe-AIO
+		echo Direct link: https://github.com/hossam-magdy/FB-TimeSaver/archive/Ahk2Exe-AIO.zip
+		::start "" "https://github.com/hossam-magdy/FB-TimeSaver/releases/tag/Ahk2Exe-AIO"
+		::start "" "https://github.com/hossam-magdy/FB-TimeSaver/archive/Ahk2Exe-AIO.zip"
+		echo.
+	)
+)
